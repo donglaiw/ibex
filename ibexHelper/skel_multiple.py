a3b93d240fa3b2973ee8e7cccbaa26c2d1e08f0b
donglaiw <donglai@holylogin01.rc.fas.harvard.edu>
2021-02-10 17:53:25 -0500

    gen multiple skel

--- ibexHelper/skel_multiple.py
+++ ibexHelper/skel_multiple.py
@@ -10,26 +10,27 @@ from ibex.utilities.dataIO import ReadH5File,ReadSkeletons
     -> complete path to the output folder
     seg_path (type string)
     -> complete path to the segmentation file (file must be in .h5 format)
-    dsmpl_res (type array)
+    resolution (type array)
     -> array of size 3 having resolution(in nm) of segmentation
 """
 
-def generate_skels(output_path, seg_path, dsmpl_res=(80,80,80)):
+def generate_skels(output_path, seg_path, resolution, ds_resolution = (80,80,80)):
     meta_path = output_path + "/meta.txt"
     segmentation = ReadH5File(seg_path, "main").astype(np.int64)
     grid = [len(segmentation), len(segmentation[0]), len(segmentation[0][0])]
+    # order: xyz
     with open(meta_path, "w") as meta_file:
         meta_file.write("# resolution in nm\n")
-        meta_file.write("%dx%dx%d\n"%(dsmpl_res[0],dsmpl_res[1],dsmpl_res[2]))
+        meta_file.write("%dx%dx%d\n"%(resolution[2],resolution[1],resolution[0]))
         meta_file.write("# mask filename\n")
         meta_file.write("%s main\n"%seg_path)
         meta_file.write("# grid size\n")
-        meta_file.write("%dx%dx%d\n"%(grid[0],grid[1],grid[2]))
+        meta_file.write("%dx%dx%d\n"%(grid[2],grid[1],grid[0]))
 
-    DownsampleMapping(output_path, segmentation, output_resolution=dsmpl_res)
-    TopologicalThinning(output_path, segmentation, skeleton_resolution=dsmpl_res)
-    FindEndpointVectors(output_path, skeleton_resolution=dsmpl_res)
-    FindEdges(output_path, skeleton_resolution=dsmpl_res)
+    DownsampleMapping(output_path, segmentation, output_resolution=ds_resolution)
+    TopologicalThinning(output_path, segmentation, skeleton_resolution=ds_resolution)
+    FindEndpointVectors(output_path, skeleton_resolution=ds_resolution)
+    FindEdges(output_path, skeleton_resolution=ds_resolution)
     skeletons = ReadSkeletons(output_path)
     
     with open(output_path + 'skel.pkl', 'wb') as fp:
